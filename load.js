63b294dacad4cd1369b3857b3e22043f8a0f9efc
Devstmike <139519652+Devstmike@users.noreply.github.com>
2023-10-31 09:14:56 +0100

    update load.js

--- load.js
+++ load.js
@@ -3,18 +3,18 @@ const myright = document.querySelector(".mainContent .column2");
 const leftcontent = `<h1><i class="fa-brands fa-x-twitter"></i></h1>
 <ul>
     <li><a href="index.html"><i class="fa-solid fa-house"></i><span>Home</span></a></li>
-    <li><a href=""><i class="fa-solid fa-magnifying-glass"></i><span>Explore</span></a></li>
+    <li><a href="message.html"><i class="fa-solid fa-magnifying-glass"></i><span>Explore</span></a></li>
     <li><a href="notification.html"><i class="fa-regular fa-bell"></i><span>Notifications</span></a></li>
     <li><a href=""><i class="fa-regular fa-envelope"></i><span>Messages</span></a></li>
     <li><a href=""><i class="fa-solid fa-list"></i><span>Lists</span></a></li>
     <li><a href=""><i class="fa-solid fa-user-group"></i><span>Communities</span></a></li>
-    <li><a href=""><i class="fa-brands fa-x-twitter"></i><span>Premium</span></a></li>
+    <li class="premiumpage"><a href="premium.html"><i class="fa-brands fa-x-twitter"></i><span>Premium</span></a></li>
     <li><a href=""><i class="fa-solid fa-user-large"></i><span>Profile</span></a></li>
     <li><a href=""><i class="fa-regular fa-face-meh-blank"></i><span>More</span></a></li>
 </ul>
 <p>Post</p>
 <div class="profile">
-    <img src=".\images\myprofilepic.jpg" alt="">
+    <img src="./images/myprofilepic.jpg" alt="">
     <div class="text">
         <h3>SpreadsheetLord</h3>    
          <p>@<span>SpreadsheetLord</span></p>
